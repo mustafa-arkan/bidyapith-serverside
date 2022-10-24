edbff7132c6d9ea3ca75363651bba99bd760183e
mustafa-arkan <arkancse@gmail.com>
2022-10-25 00:19:22 +0600

    server basic  setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const app=express()
+const port=process.env.PORT||5000
+
+app.get('/',(req,res)=>{
+
+res.send('This is bidyapith server...yay')
+
+})
+
+app.listen(port,()=>{
+
+
+console.log(`server running on port: ${port}`)
+
+})
