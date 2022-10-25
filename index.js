0c736ba100aaa16054efd3c1445790c655d53166
mustafa-arkan <arkancse@gmail.com>
2022-10-26 00:37:21 +0600

    json file update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const categories=require('./data/courselist.json')
 
 app.get('/',(req,res)=>{
 
-res.send('This is bidyapith server...yay')
+res.send('This is bidyapith server...yay yay')
 
 })
 
@@ -23,6 +23,41 @@ res.send(categories)
 
 
 })
+
+app.get('/category/:id',(req,res)=>{
+
+const id=req.params.id
+
+const category_news=categories.filter(n=>n.category_id===id)
+res.send(category_news)
+//console.log(req.params.id)
+
+
+})
+
+
+
+
+
+
+
+
+
+// app.get('/news/:id',(req,res)=>{
+
+// const id=req.params.id
+
+// const selectedNews=categories.find(n=>n.id===id)
+// res.send(selectedNews)
+// //console.log(req.params.id)
+
+
+// })
+
+
+
+
+
 
 
 
